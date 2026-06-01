61de676dd0693089e3e553c5df0b2ed5f07624bb
shobhitchaubey01 <shobhitchoubey01@gmail.com>
2026-06-01 21:54:18 +0530

    Add files via upload

---
+++ Question4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n,count=0;
+    printf("Enter the number :");
+    scanf("%d",&n);
+    while(n!=0){
+        count=count+1;
+        n=n/10;
+    }
+    printf("number of digit is %d",count);
+    return 0;
+}
