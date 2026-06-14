56f781dc631ee6554f9a599a36ad0260f5531506
shobhitchaubey01 <shobhitchoubey01@gmail.com>
2026-06-14 09:37:07 +0530

    Update Question8.c

--- Question8.c
+++ Question8.c
@@ -8,9 +8,9 @@ int main()
     num = n;
     while (n!=0)
     {
-        r=n%10;
+        r = n % 10;
         rev = rev * 10 + r;
-        n=n/10;
+        n =n / 10;
     }
     if(num==rev)
     {
